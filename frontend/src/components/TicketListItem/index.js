c920de010599f17a6cdb9b396be9c8e507191029
Douglas <94370762+dgstx@users.noreply.github.com>
2024-11-25 15:50:07 -0300

    fix color

--- frontend/src/components/TicketListItem/index.js
+++ frontend/src/components/TicketListItem/index.js
@@ -120,6 +120,7 @@ const useStyles = makeStyles(theme => ({
 		justifySelf: "flex",
 		transform: "scale(1) translate(20%, -50%)",
 		marginRight: 10,
+		backgroundColor: '#ff5e43',
 	},
 	contactLastMessage: {
 		paddingRight: 20,
