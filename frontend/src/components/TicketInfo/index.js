375b3abecc13827c1ac07dc7a40b776e59322bbd
Douglas <94370762+dgstx@users.noreply.github.com>
2024-11-30 02:05:06 -0300

    fix

--- frontend/src/components/TicketInfo/index.js
+++ frontend/src/components/TicketInfo/index.js
@@ -1,38 +1,53 @@
 import React from "react";
-
 import { Avatar, CardHeader } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { i18n } from "../../translate/i18n";
 
-const useStyles = makeStyles(() => ({
-	avatar: {
-		width: "55px",
-		height: "55px",
-		borderRadius: "25%"
-	}
+const useStyles = makeStyles((theme) => ({
+    avatar: {
+        width: "55px",
+        height: "55px",
+        borderRadius: "25%",
+    },
+    cardHeader: {
+        cursor: "pointer",
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: "1.2rem",
+    },
+    subheader: {
+        display: "flex",
+        alignItems: "center",
+    },
 }));
 
 const TicketInfo = ({ contact, ticket, onClick }) => {
-	const classes = useStyles();
-	return (
-		<CardHeader
-			onClick={onClick}
-			style={{ cursor: "pointer" }}
-			titleTypographyProps={{
-				noWrap: true,
-				style: { fontWeight: "bold", fontSize: "1.2rem" } // Negrito e cor preta
-			}}
-			subheaderTypographyProps={{ noWrap: true }}
-			avatar={<Avatar src={contact.profilePicUrl} className={classes.avatar} alt="contact_image" />}
-			title={contact.name}
-			subheader={
-				`Ticket: #${ticket.id} | ` + // Número do ticket como primeiro item
-				(ticket.user &&
-					`${i18n.t("messagesList.header.assignedTo")} ${ticket.user.name} 
-					${ticket.queue ? ' | Setor: ' + ticket.queue.name : ' | Setor: Nenhum'}`)
-			}
-		/>
-	);
+    const classes = useStyles();
+    return (
+        <CardHeader
+            onClick={onClick}
+            className={classes.cardHeader}
+            titleTypographyProps={{
+                noWrap: true,
+                className: classes.title,
+            }}
+            subheaderTypographyProps={{ noWrap: true }}
+            avatar={<Avatar src={contact.profilePicUrl} className={classes.avatar} alt="contact_image" />}
+            title={contact.name}
+            subheader={
+                <div className={classes.subheader}>
+                    {`Ticket: #${ticket.id} | `}
+                    {ticket.user && (
+                        <>
+                            {`${i18n.t("messagesList.header.assignedTo")} ${ticket.user.name}`}
+                            {ticket.queue ? ` | Setor: ${ticket.queue.name}` : " | Setor: Nenhum"}
+                        </>
+                    )}
+                </div>
+            }
+        />
+    );
 };
 
 export default TicketInfo;
--- frontend/src/components/TicketOptionsMenu/index.js
+++ frontend/src/components/TicketOptionsMenu/index.js
@@ -1,8 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-
+import { MenuItem, Menu, makeStyles } from "@material-ui/core";
 import { i18n } from "../../translate/i18n";
 import api from "../../services/api";
 import ConfirmationModal from "../ConfirmationModal";
@@ -11,92 +8,96 @@ import toastError from "../../errors/toastError";
 import { Can } from "../Can";
 import { AuthContext } from "../../context/Auth/AuthContext";
 
+const useStyles = makeStyles((theme) => ({
+    menu: {
+        borderRadius: 13,
+    },
+}));
+
 const TicketOptionsMenu = ({ ticket, menuOpen, handleClose, anchorEl }) => {
-	const [confirmationOpen, setConfirmationOpen] = useState(false);
-	const [transferTicketModalOpen, setTransferTicketModalOpen] = useState(false);
-	const isMounted = useRef(true);
-	const { user } = useContext(AuthContext);
+    const classes = useStyles();
+    const [confirmationOpen, setConfirmationOpen] = useState(false);
+    const [transferTicketModalOpen, setTransferTicketModalOpen] = useState(false);
+    const isMounted = useRef(true);
+    const { user } = useContext(AuthContext);
 
-	useEffect(() => {
-		return () => {
-			isMounted.current = false;
-		};
-	}, []);
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        };
+    }, []);
 
-	const handleDeleteTicket = async () => {
-		try {
-			await api.delete(`/tickets/${ticket.id}`);
-		} catch (err) {
-			toastError(err);
-		}
-	};
+    const handleDeleteTicket = async () => {
+        try {
+            await api.delete(`/tickets/${ticket.id}`);
+        } catch (err) {
+            toastError(err);
+        }
+    };
 
-	const handleOpenConfirmationModal = e => {
-		setConfirmationOpen(true);
-		handleClose();
-	};
+    const handleOpenConfirmationModal = (e) => {
+        setConfirmationOpen(true);
+        handleClose();
+    };
 
-	const handleOpenTransferModal = e => {
-		setTransferTicketModalOpen(true);
-		handleClose();
-	};
+    const handleOpenTransferModal = (e) => {
+        setTransferTicketModalOpen(true);
+        handleClose();
+    };
 
-	const handleCloseTransferTicketModal = () => {
-		if (isMounted.current) {
-			setTransferTicketModalOpen(false);
-		}
-	};
+    const handleCloseTransferTicketModal = () => {
+        if (isMounted.current) {
+            setTransferTicketModalOpen(false);
+        }
+    };
 
-	return (
-		<>
-			<Menu
-				id="menu-appbar"
-				anchorEl={anchorEl}
-				getContentAnchorEl={null}
-				anchorOrigin={{
-					vertical: "bottom",
-					horizontal: "right",
-				}}
-				keepMounted
-				transformOrigin={{
-					vertical: "top",
-					horizontal: "right",
-				}}
-				open={menuOpen}
-				onClose={handleClose}
-			>
-				<MenuItem onClick={handleOpenTransferModal}>
-					{i18n.t("ticketOptionsMenu.transfer")}
-				</MenuItem>
-				<Can
-					role={user.profile}
-					perform="ticket-options:deleteTicket"
-					yes={() => (
-						<MenuItem onClick={handleOpenConfirmationModal}>
-							{i18n.t("ticketOptionsMenu.delete")}
-						</MenuItem>
-					)}
-				/>
-			</Menu>
-			<ConfirmationModal
-				title={`${i18n.t("ticketOptionsMenu.confirmationModal.title")}${
-					ticket.id
-				} ${i18n.t("ticketOptionsMenu.confirmationModal.titleFrom")} ${
-					ticket.contact.name
-				}?`}
-				open={confirmationOpen}
-				onClose={setConfirmationOpen}
-				onConfirm={handleDeleteTicket}
-			>
-				{i18n.t("ticketOptionsMenu.confirmationModal.message")}
-			</ConfirmationModal>
-			<TransferTicketModal
-				modalOpen={transferTicketModalOpen}
-				onClose={handleCloseTransferTicketModal}
-				ticketid={ticket.id}
-			/>
-		</>
-	);
+    return (
+        <>
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                getContentAnchorEl={null}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                }}
+                open={menuOpen}
+                onClose={handleClose}
+                classes={{ paper: classes.menu }}
+            >
+                <MenuItem onClick={handleOpenTransferModal}>
+                    {i18n.t("ticketOptionsMenu.transfer")}
+                </MenuItem>
+                <Can
+                    role={user.profile}
+                    perform="ticket-options:deleteTicket"
+                    yes={() => (
+                        <MenuItem onClick={handleOpenConfirmationModal}>
+                            {i18n.t("ticketOptionsMenu.delete")}
+                        </MenuItem>
+                    )}
+                />
+            </Menu>
+            <ConfirmationModal
+                title={`${i18n.t("ticketOptionsMenu.confirmationModal.title")}${ticket.id} ${i18n.t("ticketOptionsMenu.confirmationModal.titleFrom")} ${ticket.contact.name}?`}
+                open={confirmationOpen}
+                onClose={setConfirmationOpen}
+                onConfirm={handleDeleteTicket}
+            >
+                {i18n.t("ticketOptionsMenu.confirmationModal.message")}
+            </ConfirmationModal>
+            <TransferTicketModal
+                modalOpen={transferTicketModalOpen}
+                onClose={handleCloseTransferTicketModal}
+                ticketid={ticket.id}
+            />
+        </>
+    );
 };
 
 export default TicketOptionsMenu;
