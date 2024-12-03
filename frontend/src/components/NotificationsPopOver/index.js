ac8ba1aad57a920d9449b65113d7c5c8254414ce
Douglas <94370762+dgstx@users.noreply.github.com>
2024-12-03 03:09:03 -0300

    remove mute

--- frontend/src/components/NotificationsPopOver/index.js
+++ frontend/src/components/NotificationsPopOver/index.js
@@ -8,8 +8,6 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Popover from "@material-ui/core/Popover";
 import { makeStyles } from "@material-ui/core/styles";
 import ChatIcon from "@material-ui/icons/Chat";
-import VolumeOffIcon from "@material-ui/icons/VolumeOff";
-import VolumeUpIcon from "@material-ui/icons/VolumeUp";
 import { format } from "date-fns";
 import { useHistory } from "react-router-dom";
 import useSound from "use-sound";
@@ -50,13 +48,10 @@ const NotificationsPopOver = () => {
 	const anchorEl = useRef();
 	const [isOpen, setIsOpen] = useState(false);
 	const [notifications, setNotifications] = useState([]);
-	const [isAudioEnabled, setIsAudioEnabled] = useState(
-		localStorage.getItem("userAudioEnabled") === "true"
-	);
 	const [, setDesktopNotifications] = useState([]);
 
 	const { tickets } = useTickets({ withUnreadMessages: "true" });
-	const [play, { stop }] = useSound(alertSound, { soundEnabled: isAudioEnabled });
+	const [play] = useSound(alertSound, { soundEnabled: true });
 	const soundAlertRef = useRef();
 
 	const historyRef = useRef(history);
@@ -111,10 +106,10 @@ const NotificationsPopOver = () => {
 			return [notification, ...prevState];
 		});
 
-		if (isAudioEnabled && soundAlertRef.current) {
+		if (soundAlertRef.current) {
 			soundAlertRef.current();
 		}
-	}, [isAudioEnabled]);
+	}, []);
 
 	useEffect(() => {
 		const socket = openSocket();
@@ -180,16 +175,6 @@ const NotificationsPopOver = () => {
 		};
 	}, [user, handleNotifications]);
 
-	const toggleAudio = () => {
-		const newAudioStatus = !isAudioEnabled;
-		setIsAudioEnabled(newAudioStatus);
-		localStorage.setItem("userAudioEnabled", newAudioStatus.toString());
-
-		if (!newAudioStatus) {
-			stop();
-		}
-	};
-
 	const handleClick = () => {
 		setIsOpen(prevState => !prevState);
 	};
@@ -204,9 +189,6 @@ const NotificationsPopOver = () => {
 
 	return (
 		<>
-			<IconButton onClick={toggleAudio} aria-label="Toggle Sound" color="inherit">
-				{isAudioEnabled ? <VolumeUpIcon /> : <VolumeOffIcon />}
-			</IconButton>
 			<IconButton
 				onClick={handleClick}
 				ref={anchorEl}
